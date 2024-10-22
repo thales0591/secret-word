fa16c75565d2aa6d108b3b3c1f24eefb686f7387
thales0591 <thales0591@gmail.com>
2024-10-22 13:45:14 -0300

    :sparkles: feat: create components structure and game page css

--- src/App.jsx
+++ src/App.jsx
@@ -18,12 +18,47 @@ function App() {
   const [gameStage, setGameStage] = useState(stages[0].name)
   const [words] = useState(wordsList)
 
+  const [pickedWord, setPickedWord] = useState("")
+  const [pickedCatergory, setPickedCategory] = useState("")
+  const [letters, setLetters] = useState([])
+
+  const pickWordAndCategory = () => {
+    const categories = Object.keys(words)
+    const category = categories[Math.floor(Math.random() * categories.length)]
+
+    const word = words[category][Math.floor(Math.random() * words[category].length)]
+   
+    return { word, category }
+  }
+
+  const startGame = () => {
+    const {word, category} = pickWordAndCategory()
+
+    let wordLetters = word.split("")
+
+    wordLetters = wordLetters.map((l) => (l.toLowerCase()))
+
+    setPickedWord(word)
+    setPickedCategory(category)
+    setLetters(wordLetters)
+
+    setGameStage(stages[1].name)
+  }
+
+  const verifyLetter = () => {
+    setGameStage(stages[2].name)
+  }
+
+  const retry = () => {
+    setGameStage(stages[0].name)
+  }
+
   return (
     <>
       <div className='App'>
-        {gameStage === "start" && <StartScreen/>}
-        {gameStage === "start" && <Game/>}
-        {gameStage === "start" && <GameOver/>}
+        {gameStage === "start" && <StartScreen startGame={startGame}/>}
+        {gameStage === "game" && <Game verifyLetter={verifyLetter}/>}
+        {gameStage === "end" && <GameOver retry={retry}/>}
       </div>
     </>
   )
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,8 +1,33 @@
 import './Game.css'
 
-const Game = () => {
+const Game = ({verifyLetter}) => {
   return (
-    <div>Game</div>
+    <div>
+        <p className='points'>
+            <span>Pontuação: 000</span>
+        </p>
+        <h1>Advinhe a palavra: </h1>
+        <h3 className='tip'>
+            Dica sobre a palavra: <span>Dica...</span>
+        </h3>
+        <p>Você ainda tem xx tentativa(s).</p>
+        <div className="wordContainer">
+            <span className='letter'>a</span>
+            <span className="blankSquare"></span>
+        </div>
+        <div className="letterContainer">
+            <p>Tente advinhar uma letra da palavra:</p>
+            <form>
+                <input type="text" name='letter' maxLength="1" required />
+                <button>Jogar!</button>
+            </form>
+        </div>
+        <div className="wrongLettersContainer">
+            <p>Letras já utilizadas:</p>
+            <span>a, </span>
+            <span>b, </span>
+        </div>
+    </div>
   )
 }
 
